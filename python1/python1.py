610b83eb3ecf72c1650b232ffa3b4ed851ca469a
roggero <roggero@eurecom.fr>
2018-11-26 15:30:24 +0100

    Added shell1 & shell2

--- python1/python1.py
+++ python1/python1.py
@@ -1,11 +1,14 @@
 import sys
+import re
 
 def print_file(file_name):
 	f=open(file_name)
 	for line in f:
 		print line
+		result = re.search('from ([0-9\.]+) port', line)
+		print result.group(1)
 	f.close
 
 if __name__ == "__main__":
 	file=(sys.argv[1])
-	print_file()
+	print_file(file)
